c0d4d344cac996891e94856b1eaa60eb2c20d19d
Shashika Mahindapala <shashika@mdidev.com>
2023-09-05 10:10:20 -0300

    added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import './App.css';
 import {ThemeProvider} from "@mui/material";
 import {RouterProvider} from "react-router-dom";
 import {router} from "./navigation/route";
-import {useSelector} from "react-redux";
+
+import {useAppSelector} from "./state/hook";
 import Light from "./themes/light";
 import Dark from "./themes/dark";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+
+import '@fontsource/roboto/700.css';
+
 function App() {
-    const themeType = useSelector<any>(state => state.app.theme);
+    const themeType = useAppSelector(state => state.app.theme);
     return (
         <ThemeProvider theme={themeType === 'light' ? Light : Dark}>
             <RouterProvider router={router()}/>
--- src/components/organism/layout/footer/Footer.tsx
+++ src/components/organism/layout/footer/Footer.tsx
@@ -1,2 +1,12 @@
 import CopyrightIcon from '@mui/icons-material/Copyright';
-export const Footer = () => <div>Built by Shashika <CopyrightIcon/>2023</div>;
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+export const Footer = () => {
+    return <Container>
+        <Box my={3}>
+            <Typography>Built by Shashika 2023<CopyrightIcon fontSize='small'/></Typography>
+        </Box>
+    </Container>;
+}
